430a516e66eb39a068dbfda93c66ea8c50eb1ac0
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-27 12:39:13 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,7 +20,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 
 def won?(board)
-  WIN_COMBINATIONS.each  { |win_comb|
+  WIN_COMBINATIONS.each { |win_comb|
     win_index_0=win_comb[0]
     win_index_1=win_comb[1]
     win_index_2=win_comb[2]
