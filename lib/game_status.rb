ead66e2775772d7d8600ed80c5be50043d4616d2
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-27 13:06:59 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,4 +21,19 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def won?(board)
   WIN_COMBINATIONS.each  |win_comb|
-    
+      win_index_0 = win_comb[0]
+   win_index_1 = win_comb[1]
+    win_index_2 = win_comb[2]
+
+     pos_1 = board[win_index_0]
+    pos_2 = board[win_index_1]
+    pos_3 = board[win_index_2]
+
+     if pos_1 == "X" && pos_2 == "X" && pos_3 == "X"
+      return win_comb
+    elsif pos_1 == "O" && pos_2 == "O" && pos_3 == "O"
+      return win_comb
+    end
+  
+  return false
+end
